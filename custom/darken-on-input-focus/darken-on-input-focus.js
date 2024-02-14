6150692fba89c16e28bd69235df689e1b9f1a7f0
LONG ZEYU <2565583595long@gmail.com>
2024-02-14 17:32:36 +0800

    Site updated: 2024-02-14 17:32:36

--- custom/darken-on-input-focus/darken-on-input-focus.js
+++ custom/darken-on-input-focus/darken-on-input-focus.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const targetInputSelectors = ['input', 'textarea', '#wl-nick', '#wl-mail', '#wl-link', '#wl-edit']; // 更新为特定的输入框选项
-    const bodyDarkenClass = 'body-darken'; // 当输入框聚焦时，给body添加的class
+    const bodyDarkenClass = 'body-darken'; 
 
-    // 功能：为页面体添加一个遮罩层的样式
     function toggleBodyDarken(on) {
         if (on) {
             document.body.classList.add(bodyDarkenClass);
@@ -11,11 +9,16 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    // 为所有目标输入框添加事件监听器
-    targetInputSelectors.forEach(selector => {
-        document.querySelectorAll(selector).forEach(inputElement => {
-            inputElement.addEventListener('focus', () => toggleBodyDarken(true));
-            inputElement.addEventListener('blur', () => toggleBodyDarken(false));
-        });
+    // 使用事件委托
+    document.getElementById('comments').addEventListener('focusin', function(event) {
+        if (event.target.matches('#wl-nick, #wl-mail, #wl-link, #wl-edit')) {
+            toggleBodyDarken(true);
+        }
+    });
+
+    document.getElementById('comments').addEventListener('focusout', function(event) {
+        if (event.target.matches('#wl-nick, #wl-mail, #wl-link, #wl-edit')) {
+            toggleBodyDarken(false);
+        }
     });
 });
