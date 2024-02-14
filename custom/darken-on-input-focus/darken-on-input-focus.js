fb9246c89d04d492217e9dbcd905e38e8603aa0f
LONG ZEYU <2565583595long@gmail.com>
2024-02-14 18:02:00 +0800

    Site updated: 2024-02-14 18:02:00

--- custom/darken-on-input-focus/darken-on-input-focus.js
+++ custom/darken-on-input-focus/darken-on-input-focus.js
@@ -1,22 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const overlay = document.createElement('div');
-    overlay.className = 'overlay-darken';
-    document.body.appendChild(overlay);
-
-    const commentsContainer = document.getElementById('comments');
-
-    function toggleOverlay(on) {
-        overlay.style.display = on ? 'block' : 'none';
+    // 检查页面上是否已经存在遮罩层，否则创建它
+    let overlay = document.querySelector('.overlay-darken');
+    if (!overlay) {
+        overlay = document.createElement('div');
+        overlay.className = 'overlay-darken';
+        document.body.appendChild(overlay);
     }
 
-    commentsContainer.addEventListener('focusin', function(event) {
-        // 检查事件的目标是否为我们关心的输入框
-        if (event.target.matches('#wl-nick, #wl-mail, #wl-link, #wl-edit')) {
-            toggleOverlay(true); // 显示遮罩层
-        }
-    });
+    // 为评论输入框添加 focusin 和 focusout 事件监听器
+    const commentsContainer = document.getElementById('comments');
+    function toggleOverlay(display) { 
+        overlay.style.display = display ? 'block' : 'none'; // 显示或隐藏遮罩层
+    }
 
-    commentsContainer.addEventListener('focusout', function(event) {
-        toggleOverlay(false); // 隐藏遮罩层
-    });
+    commentsContainer.addEventListener('focusin', () => toggleOverlay(true));
+    commentsContainer.addEventListener('focusout', () => toggleOverlay(false));
 });
