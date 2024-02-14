00d59758e54bf5c82750c841f68ce47210b7781a
LONG ZEYU <2565583595long@gmail.com>
2024-02-14 17:28:37 +0800

    Site updated: 2024-02-14 17:28:36

--- custom/darken-on-input-focus/darken-on-input-focus.js
+++ custom/darken-on-input-focus/darken-on-input-focus.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const targetInputSelectors = ['#wl-nick', '#wl-mail', '#wl-link', '#wl-edit']; // 更新为特定的输入框选项
+    const targetInputSelectors = ['input', 'textarea', '#wl-nick', '#wl-mail', '#wl-link', '#wl-edit']; // 更新为特定的输入框选项
     const bodyDarkenClass = 'body-darken'; // 当输入框聚焦时，给body添加的class
 
     // 功能：为页面体添加一个遮罩层的样式
